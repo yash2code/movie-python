e22d898cec83a7e673f04216d762096831b379cc
PriNce YaSh <PriNce YaSh>
2017-06-13 23:25:17 +0530

    pep8 fix for media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,51 +1,65 @@
-import os                                                   # For importing modules in the program              
-__path__=[os.path.dirname(os.path.abspath(__file__))]       # For setting directory for the module (found on stackoverflow.com)
-from . import media           # class module for Movie
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# For importing modules in the program
+
+import os
+
+# For setting directory for the module (found on stackoverflow.com)
+
+__path__ = [os.path.dirname(os.path.abspath(__file__))]
+from . import media  # class module for Movie
 from . import fresh_tomatoes  # template for the webpage
 
-
 # Iron Man
-Iron_Man = media.Movie("Iron Man",
-                        'A wealthy American business magnate, playboy, and ingenious engineer,Tony Stark suffers a severe chest injury during a kidnapping in which... ',
-                        "https://s-media-cache-ak0.pinimg.com/736x/e5/e8/a7/e5e8a744ab691f61a4afe09bdf36d8aa.jpg",
-						"https://www.youtube.com/watch?v=8hYlB38asDY")
+
+Iron_Man = media.Movie('Iron Man',
+                       'A wealthy American business magnate, playboy, and ingenious engineer,Tony Stark suffers a severe chest injury during a kidnapping in which... ',
+                       'https://s-media-cache-ak0.pinimg.com/736x/e5/e8/a7/e5e8a744ab691f61a4afe09bdf36d8aa.jpg', 'https://www.youtube.com/watch?v=8hYlB38asDY')
 
 # Harry Potter DH part 2
-Harry_Potter = media.Movie("Harry Potter ",
-						   'Harry, Ron and Hermione face a race against time to destroy the remaining horcruxes',
-                        "http://img.moviepostershop.com/harry-potter-and-the-deathly-hallows-part-ii-movie-poster-2011-1010702626.jpg",
-						"https://www.youtube.com/watch?v=mObK5XD8udk")
+
+Harry_Potter = media.Movie('Harry Potter ',
+                           'Harry, Ron and Hermione face a race against time to destroy the remaining horcruxes',
+                           'http://img.moviepostershop.com/harry-potter-and-the-deathly-hallows-part-ii-movie-poster-2011-1010702626.jpg',
+                           'https://www.youtube.com/watch?v=mObK5XD8udk'
+                           )
 
 # Dark_Knight_Rises
-Dark_Knight = media.Movie("The Dark Knight Rises",
-                       'Bane, an imposing terrorist, attacks Gotham City and disrupts its eight-year-long period of peace',
-                        "http://www.moviesonline.ca/wp-content/uploads/2012/07/the-dark-knight-rises-imax.jpeg",
-						"https://www.youtube.com/watch?v=g8evyE9TuYk")
+
+Dark_Knight = media.Movie('The Dark Knight Rises',
+                          'Bane, an imposing terrorist, attacks Gotham City and disrupts its eight-year-long period of peace',
+                          'http://www.moviesonline.ca/wp-content/uploads/2012/07/the-dark-knight-rises-imax.jpeg',
+                          'https://www.youtube.com/watch?v=g8evyE9TuYk')
 
 # Bahubali 2
-Bahubali = media.Movie("Baahubali 2 - The Conclusion",
+
+Bahubali = media.Movie('Baahubali 2 - The Conclusion',
                        'Kattappa (Sathyaraj) continues to narrate how he ended up killing Amarendra Baahubali (Prabhas)',
-                        "https://www.desiretrees.in/wp-content/uploads/2017/02/Baahubali-2-New-Poster-Maha-Shivaratri.jpg",
-						"https://www.youtube.com/watch?v=G62HrubdD6o")
+                       'https://www.desiretrees.in/wp-content/uploads/2017/02/Baahubali-2-New-Poster-Maha-Shivaratri.jpg', 'https://www.youtube.com/watch?v=G62HrubdD6o')
 
 # Dhoom 2
-Dhoom = media.Movie("Dhoom 2",
-                       'Mr. A, a fearless thief, steals valuable artefacts',
-                        "http://www.impawards.com/intl/india/2006/posters/dhoom_2_xlg.jpg",
-						"https://www.youtube.com/watch?v=gUelnhpgUQ4")
+
+Dhoom = media.Movie('Dhoom 2',
+                    'Mr. A, a fearless thief, steals valuable artefacts',
+                    'http://www.impawards.com/intl/india/2006/posters/dhoom_2_xlg.jpg', 'https://www.youtube.com/watch?v=gUelnhpgUQ4')
 
 # 3 Idiots
-idiots = media.Movie("3 idiots",
-                       'In college, Farhan and Raju form a great bond with Rancho due to his refreshing outlook. Years later, a bet gives them a chance to look for their long-lost friend whose existence seems rather elusive.',
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BZWRlNDdkNzItMzhlZC00YTdmLWIwNjktYjY5NjQ1ZmQ3N2FkXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR133,0,630,1200_AL_.jpg",
-						"https://www.youtube.com/watch?v=xvszmNXdM4w")
 
+idiots = media.Movie('3 idiots',
+                     'In college, Farhan and Raju form a great bond with Rancho due to his refreshing outlook. Years later, a bet gives them a chance to look for their long-lost friend whose existence seems rather elusive.',
+                     'https://images-na.ssl-images-amazon.com/images/M/MV5BZWRlNDdkNzItMzhlZC00YTdmLWIwNjktYjY5NjQ1ZmQ3N2FkXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR133,0,630,1200_AL_.jpg', 'https://www.youtube.com/watch?v=xvszmNXdM4w')
 
+movies = [  # list to pass in the open_movies_page method
+    Iron_Man,
+    Harry_Potter,
+    Dark_Knight,
+    Bahubali,
+    Dhoom,
+    idiots,
+]
 
-movies = [Iron_Man,Harry_Potter,Dark_Knight,Bahubali,Dhoom,idiots]  # list to pass in the open_movies_page method
+# exporting the list to template for generating webpage
 
-fresh_tomatoes.open_movies_page(movies)       # exporting the list to template for generating webpage
-
-
+fresh_tomatoes.open_movies_page(movies)
 
 print(media.__doc__)
--- media.py
+++ media.py
@@ -1,25 +1,28 @@
 
 """
- This class module gives all the information of the instance created for a movie.
-		
-		Attributes:
-			movie_title: It is the title of the movie.
-			movie_storyline: It is the description of the movie.
-			poster_image: It is the art or poster of the movie.
-			trailer_youtube: It is the video of the trailer for the movie.
+ This class module gives all the information of the instance...
+  ...created for a movie.
+
+    Attributes:
+      movie_title: It is the title of the movie.
+      movie_storyline: It is the description of the movie.
+      poster_image: It is the art or poster of the movie.
+      trailer_youtube: It is the video of the trailer for the movie.
 """
 import webbrowser
+
+
 class Movie():
 
-    
-       
-        def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-            """Constructor of the class."""
-            self.title = movie_title
-            self.storyline = movie_storyline
-            self.poster_image_url = poster_image
-            self.trailer_youtube_url = trailer_youtube
-            
-        def show_trailer(self):
-           """It is a method which opens the trailer of the movie in a browser window."""
-           webbrowser.open(self.trailer_youtube_url)
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        """Constructor of the class."""
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """It is a method which opens the trailer of the movie..
+        ...in a browser window."""
+        webbrowser.open(self.trailer_youtube_url)
