64bbe0aba7446626a53b264fb115b05b9c51cfd0
PriNce YaSh <PriNce YaSh>
2017-06-13 19:19:30 +0530

    update media.py docstring

--- media.py
+++ media.py
@@ -1,16 +1,18 @@
-import webbrowser
 
-class Movie():
-
-    
-        """This class module gives all the information of the instance created for a movie.
+"""
+ This class module gives all the information of the instance created for a movie.
 		
 		Attributes:
 			movie_title: It is the title of the movie.
 			movie_storyline: It is the description of the movie.
 			poster_image: It is the art or poster of the movie.
 			trailer_youtube: It is the video of the trailer for the movie.
-		"""
+"""
+import webbrowser
+class Movie():
+
+    
+       
         def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
             """Constructor of the class."""
             self.title = movie_title
